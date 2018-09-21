782bc8597a6c24dc462ff955bd1482fe9949333a
Hattan Shobokshi <hattan@gmail.com>
2018-09-20 23:01:54 -0700

    checkpoint1: added IsUserJoinEvent check

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const { BotFrameworkAdapter, MemoryStorage, ConversationState } = require('botbuilder');
+const {BotFrameworkAdapter, MemoryStorage, ConversationState} = require("botbuilder");
 const restify = require('restify');
 const request = require('request');
+const adapter = new BotFrameworkAdapter({ 
+  appId: process.env.MicrosoftAppId, 
+  appPassword: process.env.MicrosoftAppPassword 
+});
+
+const conversationState = new ConversationState(new MemoryStorage());
+adapter.use(conversationState);
 
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(`${server.name} listening to ${server.url}`);
 });
 
-const adapter = new BotFrameworkAdapter({ 
-    appId: process.env.MicrosoftAppId, 
-    appPassword: process.env.MicrosoftAppPassword 
-});
-
-const conversationState = new ConversationState(new MemoryStorage());
-adapter.use(conversationState);
-
 server.post('/api/messages', async (req, res) => {
-  adapter.processActivity(req, res, async (context) => {
-    if (context.activity.type === 'message') {
-        const state = conversationState.get(context);
-        switch(context.activity.text){
-          case 'show sessions':
-            return context.sendActivity(`https://www.socalcodecamp.com/tracks`);
-          case 'tell joke':
-            var joke = await getJoke();
-            return context.sendActivity(joke);
-          default:
-            return context.sendActivity(`I didn't understand ${context.activity.text}. Can you rephrase please?`);
-        }        
+  adapter.processActivity(req,res, async context =>{
+    if(context.activity.type == "message"){
+      let state = conversationState.get(context);
+
+      switch(context.activity.text){
+        case 'show sessions':
+          return context.sendActivity(`https://www.socalcodecamp.com/tracks`);
+        case 'tell joke':
+          let joke = await getJoke();
+          return context.sendActivity(joke);
+        default:
+          return context.sendActivity(`I didn't understand "${context.activity.text}", can you rephrase?`);        
+      }
+
+    } else if (isUserJoinEvent(context)){
+      return handleNewUser(context);
     }
   });
 });
@@ -52,3 +55,21 @@ function getJoke(){
     });
   });
 }
+
+
+function isUserJoinEvent(context){
+  return context.activity.type = "conversationUpdate" && context.activity.membersAdded && context.activity.membersAdded.length > 0;
+}
+
+function handleNewUser(context){
+  let user = getNewUser(context);
+  if(user){
+    return context.sendActivity(`Hello ${user.name}. I'm your friendly demo bot.`);
+  }
+}
+
+function getNewUser(context){
+  return context.activity.membersAdded.find(member=> member.id != context.activity.recipient.id);
+}
+
+
