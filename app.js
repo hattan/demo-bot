e5dd51b2fd30ac7fda633292fe647d8b64000d79
Hattan Shobokshi <hattan@gmail.com>
2018-09-23 23:34:11 -0700

    added BotGreeting Middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { BotFrameworkAdapter, MemoryStorage, ConversationState } = require('botbuilder');
+const BotGreeting = require('botbuilder-greeting');
 const restify = require('restify');
 const Bot = require('./bot');
 
@@ -11,6 +12,11 @@ const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId, 
     appPassword: process.env.MicrosoftAppPassword 
 });
+
+adapter.use(new BotGreeting(context => {
+  return `Hello ! I'm your friendly demo bot.`;
+}));
+
 
 const conversationState = new ConversationState(new MemoryStorage());
 adapter.use(conversationState);
