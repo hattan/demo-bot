138e13ec2554b6d36321c4d87d4af8217046b272
Hattan Shobokshi <hattan@gmail.com>
2018-09-23 23:30:20 -0700

    added BotGreeting Middleware

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const {BotFrameworkAdapter, MemoryStorage, ConversationState} = require("botbuilder");
+const BotGreeting = require('botbuilder-greeting');
 const restify = require('restify');
 
 const adapter = new BotFrameworkAdapter({ 
   appId: process.env.MicrosoftAppId, 
   appPassword: process.env.MicrosoftAppPassword 
 });
+
+adapter.use(new BotGreeting(context => {
+  return `Hello ! I'm your friendly demo bot.`;
+}));
 
 const conversationState = new ConversationState(new MemoryStorage());
 adapter.use(conversationState);
@@ -23,25 +28,8 @@ server.post('/api/messages', async (req, res) => {
 
       return context.sendActivity(`${state.count} - You said "${context.activity.text}"`);
 
-    } else if (isUserJoinEvent(context)){
-      return handleNewUser(context);
-    }
+    } 
   });
 });
-
-function isUserJoinEvent(context){
-  return context.activity.type = "conversationUpdate" && context.activity.membersAdded && context.activity.membersAdded.length > 0;
-}
-
-function handleNewUser(context){
-  let user = getNewUser(context);
-  if(user){
-    return context.sendActivity(`Hello ${user.name}. I'm your friendly demo bot.`);
-  }
-}
-
-function getNewUser(context){
-  return context.activity.membersAdded.find(member=> member.id != context.activity.recipient.id);
-}
 
 
